9d478405ab6408a42f178cd4b59e9f4adcaeb443
praveenjuge <praveenjuge@gmail.com>
2017-08-12 16:28:51 +0530

    refresh update

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,23 +1,21 @@
-var refreshId = setInterval(function() {
-    $.ajax({
-        type: 'GET',
-        url: 'https://cors-anywhere.herokuapp.com/https://api.btcxindia.com/ticker_hour/',
-        crossDomain: true,
-        dataType: 'json',
-        async: 'true',
-        error: function() {
-            $('#main_inr').replaceWith("Server down. Try Later");
-        },
-        success: function(data) {
-            $('#main_inr').replaceWith(data.ask);
-            var time = new Date(data.last_traded_time),
-                h = time.getHours(),
-                m = time.getMinutes(),
-                d = time.getDate(),
-                mo = time.getMonth(),
-                y = time.getFullYear(),
-                mo = mo + 1;
-            $('#main_time').replaceWith("as of " + h + ":" + m + ", " + d + "-" + mo + "-" + y + ".");
-        }
-    });
-}, 2000);
+$.ajax({
+    type: 'GET',
+    url: 'https://cors-anywhere.herokuapp.com/https://api.btcxindia.com/ticker_hour/',
+    crossDomain: true,
+    dataType: 'json',
+    async: 'true',
+    error: function() {
+        $('#main_inr').replaceWith("Server down. Try Later");
+    },
+    success: function(data) {
+        $('#main_inr').replaceWith(data.ask);
+        var time = new Date(data.last_traded_time),
+            h = time.getHours(),
+            m = time.getMinutes(),
+            d = time.getDate(),
+            mo = time.getMonth(),
+            y = time.getFullYear(),
+            mo = mo + 1;
+        $('#main_time').replaceWith("as of " + h + ":" + m + ", " + d + "-" + mo + "-" + y + ".");
+    }
+});
